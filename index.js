1b6ba4b4ea0f13433bfa1d4d6b36fb1061452d62
Swayne <xwaynelil@gmail.com>
2026-09-19 13:02:27 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+import expess from "express";
+
+
+const app = expess();
+const PORT = 3000;
+
+
+app.get("/", (req, res)=>{
+    res.send("Hello World");
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
